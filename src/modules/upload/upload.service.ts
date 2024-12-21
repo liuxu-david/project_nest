6a3c12e2d1cfa71042c4ac4c8b119ecd8edd9399
liuxu <2982359231@qq.com>
2024-12-21 23:17:37 +0800

    feat: md5验证

--- src/modules/upload/upload.service.ts
+++ src/modules/upload/upload.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import * as path from 'path';
 import * as fs from "fs";
+import * as crypto from 'crypto'
 
 @Injectable()
 export class UploadService {
@@ -11,7 +12,6 @@ export class UploadService {
     try {
       const hasCurDir = fs.readdirSync(dirPath);
       // 如果存在则读取返回给前端（用于前端妙传）
-      console.log(hasCurDir);
       return hasCurDir.filter(item=>item.endsWith('.part')).map(item=>parseInt(item.split('.')[0],10))
     } catch (error) {
       // 不存在则返回空数组
@@ -34,7 +34,8 @@ export class UploadService {
     fs.renameSync(file.path,chunkFilePath)
   }
 
-  handleMergeFile(fileHash,totalNum,name){
+  async handleMergeFile(fileHash,totalNum,name){
+   try {
     const dirPath = path.resolve('FilesList',fileHash)
     const allFileInfo = fs.readdirSync(dirPath).filter(item=>item.endsWith('.part')).sort((a,b)=>{
       const chartA = parseInt(path.basename(a).split('.')[0],10);
@@ -53,9 +54,53 @@ export class UploadService {
       const chunkData = fs.readFileSync(readPath);
       writeStream.write(chunkData)
     }
-    writeStream.end()
+    await writeStream.end()
     // hash校验
-    // 上传OBS
+    console.log("outPath",outPath);
+    const fileMd5 = await this.handleMd5File(outPath)
+    console.log("fileMd5",fileMd5);
+    
+    if(fileMd5 === fileHash){
+      console.log("上传obs");
+      
+      // 上传OBS
+    }
     // 删除临时文件
+    await this.handleDeleteFile(dirPath)
+    return []
+   } catch (error) {
+    console.log("merge",error);
+    
+   }
+  }
+
+  // 读取文件夹下的文件，然后进行md5处理
+  handleMd5File(path){
+    return new Promise((resolve,reject)=>{
+      const hash = crypto.createHash('md5');
+      const fileStream = fs.createReadStream(path);
+      fileStream.on('data', (chunk) => {
+        hash.update(chunk);
+      });
+      fileStream.on('end', () => {
+        const fileMd5 = hash.digest('hex');
+        resolve(fileMd5)
+      });
+      fileStream.on('error', (err) => {
+        console.error(`File read error: ${err.message}`);
+        reject(err);
+      });
+    })
+  }
+  handleDeleteFile(dirPath){
+    return new Promise((resolve, reject) => {
+      fs.rm(dirPath, { recursive: true, force: true }, (err) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve('临时文件删除成功');
+        }
+      });
+    })
   }
 }
