0ab979817b6309445be4aa49703db078963e5207
liuxu <2982359231@qq.com>
2024-12-24 22:02:22 +0800

    fix: 测试

--- src/config/dev.config.ts
+++ src/config/dev.config.ts
@@ -1,9 +1,10 @@
 const devConfig = {
   oss_setting:{
-      // 配置环境变量ID。
-      // 配置环境变量SECRET。
-      // 将<YOUR-BUCKET>替换为Bucket名称。
-      bucket: 'liuxu-david'
+    // 这里没写是因为github写了推不上
+    // 配置环境变量ID。
+    // 配置环境变量SECRET。
+    // 将<YOUR-BUCKET>替换为Bucket名称。
+    bucket: 'liuxu-david'
   }
 }
 
