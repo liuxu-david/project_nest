1039f17520b9a7fa50481284fec6d5c63d1a318f
liuxu <2982359231@qq.com>
2024-12-26 00:07:12 +0800

    fix: 取消对config文件的追踪

--- src/config/index.ts
+++
@@ -1,9 +0,0 @@
-import devConfig from "./dev.config";
-import prodConfig from "./prod.config";
-
-const configMap = {
-  DEVELOPMENT: devConfig,
-  PRODECTION: prodConfig
-}
-
-export default configMap[process.env.NODE_ENV] 
